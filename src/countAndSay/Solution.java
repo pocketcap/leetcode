4f3db691f8f1c1c780d2a3b7e46e6951e8f22a8d
Yahui Wang <jolinsen@gmail.com>
2015-06-04 21:32:31 -0400

    organize

---
+++ src/countAndSay/Solution.java
@@ -0,0 +1,28 @@
+package countAndSay;
+
+public class Solution {
+	public String countAndSay(int n) {
+		if (n <= 0)
+			return null;
+		String result = "1";
+		int i = 1;
+		while (i < n) {
+			int count = 1;
+			StringBuilder sb = new StringBuilder();
+			for (int j = 1; j < result.length(); j++) {
+				if (result.charAt(j) == result.charAt(j - 1))
+					count++;
+				else {
+					sb.append(count);
+					sb.append(result.charAt(j - 1));
+					count = 1;
+				}
+			}
+			sb.append(count);
+			sb.append(result.charAt(result.length() - 1));
+			result = sb.toString();
+			i++;
+		}
+		return result;
+	}
+}
---
+++ src/pascalTriangle/Solution.java
@@ -0,0 +1,26 @@
+package pascalTriangle;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> generate(int numRows) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		if (numRows <= 0)
+			return result;
+
+		ArrayList<Integer> pre = new ArrayList<Integer>();
+		pre.add(1);
+		result.add(pre);
+		for (int i = 2; i <= numRows; i++) {
+			ArrayList<Integer> cur = new ArrayList<Integer>();
+			cur.add(1);
+			for (int j = 0; j < pre.size() - 1; j++) {
+				cur.add(pre.get(j) + pre.get(j + 1));
+			}
+			cur.add(1);
+			result.add(cur);
+			pre = cur;
+		}
+		return result;
+	}
+}
