c2350ae6d357d505d76ed24a0edb65097b1f58cd
Yahui Wang <jolinsen@gmail.com>
2015-07-15 21:26:30 -0400

    07/15/2015

---
+++ src/lowestCommonAncestor/Solution.java
@@ -0,0 +1,24 @@
+package lowestCommonAncestor;
+
+public class Solution {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || p == null || q == null)
+			return null;
+		if (Math.max(p.val, q.val) < root.val)
+			return lowestCommonAncestor(root.left, p, q);
+		else if (Math.min(p.val, q.val) > root.val)
+			return lowestCommonAncestor(root.right, p, q);
+		else
+			return root;
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
