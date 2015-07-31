a4baa0ef42fb836516301588b38fa428cb17f24b
Yahui Wang <jolinsen@gmail.com>
2015-07-30 21:34:57 -0400

    07/30/2015

---
+++ src/totalNQueens/Solution.java
@@ -0,0 +1,57 @@
+package totalNQueens;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public int totalNQueens(int n) {
+		return solveNQueens(n).size();
+	}
+
+	public List<List<String>> solveNQueens(int n) {
+		char[][] board = new char[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = '.';
+			}
+		}
+		List<List<String>> res = new ArrayList<List<String>>();
+		dfs(board, 0, res);
+		return res;
+	}
+
+	public void dfs(char[][] board, int col, List<List<String>> res) {
+		if (col == board.length) {
+			res.add(construct(board));
+			return;
+		}
+
+		for (int j = 0; j < board.length; j++) {
+			if (validate(board, col, j)) {
+				board[col][j] = 'Q';
+				dfs(board, col + 1, res);
+				board[col][j] = '.';
+			}
+		}
+	}
+
+	public boolean validate(char[][] board, int x, int y) {
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[i][j] == 'Q'
+						&& (x + j == y + i || x + y == i + j || j == y))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public List<String> construct(char[][] board) {
+		List<String> res = new ArrayList<String>();
+		for (int i = 0; i < board.length; i++) {
+			String s = new String(board[i]);
+			res.add(s);
+		}
+		return res;
+	}
+}
